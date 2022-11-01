b855c2040b542149dbed1cd28414f112286b7e4c
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-11-01 17:04:55 +0900

    04

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int i;
-	int a[SIZE] = {1,2,3,4,5};
-	int b[SIZE] = {1,2,3,4,5};
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
+int main(void) 
+{
+	int list[SIZE] = {1,2,3,4};
 	
-	if (a==b)
-		printf("같은 값\n");
-	else
-		printf("다른 값\n");
-		
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
+	
 	return 0;
+}
+
+void square_array(int a[], int size)
+{
+	int i;
+	
+	for (i=0;i<size;i++)
+		a[i]= a[i]*a[i];	
+}
+
+void print_array(int a[], int size)
+{
+	int i;
+	
+	for (i=0;i<size;i++)
+		printf("%3d", a[i]);
+	printf("\n");
 }
